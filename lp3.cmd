2119c8470f8a4fc8172abc30b89f45d7f2d2d4ad
Gustavo6046 <gugurehermann@gmail.com>
2015-08-15 14:41:54 -0300

    Update to v0.91.1 TEST

--- lp3.cmd
+++ lp3.cmd
@@ -1,8 +1,8 @@
 @echo off
-if /i %1 == -i (echo Installing LPF package %2 to %3.) else (if /i %1 == -c (echo Creating .LPF package into %3 from %2.) else (goto error))
+if /i %1 EQU -i (echo Installing LPF package %2 to %3.) else (if /i %1 EQU -c (echo Creating .LPF package into %3 from %2.) else (goto error))
 echo --
-if /i %1 == -i (unzip.exe %2.lpf -d %3) else (zip.exe -j -r %3.lpf %2)
-if /i %1 == -i (if /i %4 == -d (del /q %2))
+if /i %1 EQU -i (unzip.exe %2.lpf -d %3) else (zip.exe -j -r %3.lpf %2)
+if /i %4 EQU "-d" if /i %1 EQU -i del /q %2
 echo --
 echo Operation successfully ended.
 exit /b
