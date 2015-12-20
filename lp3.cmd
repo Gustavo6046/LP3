91da4d7cc171ed2a001f2f5bd0c814c533469618
Gustavo6046 <gugurehermann@gmail.com>
2015-12-20 16:25:37 -0200

    Fixed -d issue
    Finally! Oh my god how that issue was annoying! Many thanks to
    #Powershell people in freenode IRC.

--- lp3.cmd
+++ lp3.cmd
@@ -2,7 +2,7 @@
 if /i %1 EQU -i (echo Installing LPF package %2 to %3.) else (if /i %1 EQU -c (echo Creating .LPF package into %3 from %2.) else (goto error))
 echo --
 if /i %1 EQU -i (unzip.exe %2.lpf -d %3) else (zip.exe -j -r %3.lpf %2)
-if /i %4 EQU "-d" if /i %1 EQU -i del /q %2
+if /i %4 EQU "-d" (if /i %1 EQU -i del /q %2)
 echo --
 echo Operation successfully ended.
 exit /b
